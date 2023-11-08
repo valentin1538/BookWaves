5f6621d3e0b18add7b8260e963a7ea2fc4b84d7f
hhddno <146323678+hhddno@users.noreply.github.com>
2023-11-08 15:45:24 +0100

    Update index.php

--- php/index.php
+++ php/index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">s
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bibliothèque SIO</title>
     <!-- Bootstrap core CSS -->
