93728add9a9b7140080716420ec97725d3bc89e5
Valentin <37328458+valentin1538@users.noreply.github.com>
2023-11-29 16:28:48 +0100

    Add files via upload

--- php/BDD BIBLIO 22-11.sql
+++ php/BDD BIBLIO 22-11.sql
@@ -21,6 +21,7 @@ create table users(
     email VARCHAR(100),
     password VARCHAR(100),
     idprofil INT,
+    urlpfp VARCHAR(250),
     FOREIGN KEY (idprofil) REFERENCES profil(id)
     );
 
@@ -36,17 +37,18 @@ nom VARCHAR(35)
 CREATE TABLE livre (
 id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
 nom VARCHAR(75),
-lienimage VARCHAR(250),
-infos VARCHAR(250),
-lienepub VARCHAR(250),
+lienfiles VARCHAR(250),
+lienfolder Varchar(250),
 idauteur INT,
 idediteur INT,
 idgenre INT,
 idlangue INT,
+idcollection INT,
 FOREIGN KEY (idauteur) REFERENCES auteur(id),
 FOREIGN KEY (idediteur) REFERENCES editeur(id),
 FOREIGN KEY (idgenre) REFERENCES genre(id),
-FOREIGN KEY (idlangue) REFERENCES langue(id)
+FOREIGN KEY (idlangue) REFERENCES langue(id),
+FOREIGN KEY (idcollection) REFERENCES collection(id)
 );
 
 
@@ -55,17 +57,18 @@ id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
 idpersonne INT,
 nom VARCHAR(75),
 lienimage VARCHAR(250),
-infos VARCHAR(250),
 lienepub VARCHAR(250),
 idauteur INT,
 idediteur INT,
 idgenre INT,
 idlangue INT,
+idcollection INT, 
 FOREIGN KEY (idpersonne) REFERENCES users(id),
 FOREIGN KEY (idauteur) REFERENCES auteur(id),
 FOREIGN KEY (idediteur) REFERENCES editeur(id),
 FOREIGN KEY (idgenre) REFERENCES genre(id),
-FOREIGN KEY (idlangue) REFERENCES langue(id)
+FOREIGN KEY (idlangue) REFERENCES langue(id),
+FOREIGN KEY (idcollection) REFERENCES collection(id)
 );
 
 
@@ -99,6 +102,27 @@ nom VARCHAR(75)
 );
 
 
+#CREATION TABLE COLLECTION EDITEUR
+CREATE TABLE collection(
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+nom VARCHAR (75),
+infos VARCHAR(250),
+idediteur INT,
+FOREIGN KEY (idediteur) REFERENCES editeur(id)
+);
+
+
+#CREATION DE LA TABLE PERSONNAGE AUTEUR
+CREATE TABLE personnage(
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+nom VARCHAR(75),
+idauteur INT,
+idcollection INT,
+FOREIGN KEY (idcollection) REFERENCES collection(id),
+FOREIGN KEY (idauteur) REFERENCES auteur(id)
+);
+
+
 ###############################
 ###############################
 #####INSERTION DES DONNEES#####
@@ -107,10 +131,11 @@ nom VARCHAR(75)
 
 
 #INSERTION UTILISATEUR ADMINISTRATEUR
-INSERT INTO users VALUES (1 ,'valentin','valentin@gmail.com',SHA2('valentin', 256),4);
-INSERT INTO users VALUES (2 ,'hugo','hugo@gmail.com',SHA2('hugo', 256),3);
-INSERT INTO users VALUES (3 ,'tim','tim@gmail.com',SHA2('tim', 256),2);
-INSERT INTO users VALUES (4 ,'achille','achille@gmail.com',SHA2('achille', 256),1);
+INSERT INTO users (id,username,email,password,idprofil)  VALUES (1 ,'valentin','valentin@gmail.com',SHA2('valentin', 256),4);
+INSERT INTO users (id,username,email,password,idprofil)  VALUES (2 ,'hugo','hugo@gmail.com',SHA2('hugo', 256),3);
+INSERT INTO users (id,username,email,password,idprofil)  VALUES (3 ,'tim','tim@gmail.com',SHA2('tim', 256),2);
+INSERT INTO users (id,username,email,password,idprofil)  VALUES (4 ,'achille','achille@gmail.com',SHA2('achille', 256),1);
+
 
 #INSERTION DES PROFILS 
 INSERT INTO profil VALUES (0 ,'Invité');
@@ -121,18 +146,37 @@ INSERT INTO profil VALUES (4 ,'Administrateur');
 
 
 #INSERTION LIVRE
-INSERT INTO livre(nom,infos,idauteur,idediteur,idgenre,idlangue) VALUES ('NO_NAME','NO_INFOS','NULL','NULL','NULL','NULL');
+INSERT INTO livre(nom,idauteur,idediteur,idgenre,idlangue,idcollection) VALUES ('NO_NAME','NULL','NULL','NULL','NULL','NULL');
 #Insertion des livres
-INSERT INTO livre(nom,infos,idauteur,idediteur,idgenre,idlangue) VALUES
-('ONE PIECE','L histoire suit principalement l équipage de Chapeau de paille, mené par son capitaine Monkey D. Luffy, un jeune homme ayant mangé le fruit du Gum Gum et dont le rêve est de devenir le Roi des pirates.',2,2,2,1),
-('Comment se faire des amis', 'Ici par le biais de cette oeuvre l auteur cherche à apprendre au lecteur des manières de se faire es amis',3,3,3,1),
-('Tintin au pays des Soviets', 'Le personnage principal Tintin se rend en Russie afin de résoudre une enquête des plus passionnantes',4,4,4,1)
+INSERT INTO livre(nom,idauteur,idediteur,idgenre,idlangue,idcollection) VALUES
+('ONE PIECE T.1',2,2,2,1,2),
+('ONE PIECE T.2',2,2,2,1,2),
+('ONE PIECE T.3',2,2,2,1,2),
+('ONE PIECE T.4',2,2,2,1,2),
+('ONE PIECE T.5',2,2,2,1,2),
+('ONE PIECE T.6',2,2,2,1,2),
+('TWO PIECE',2,2,2,1,2),
+
+('Comment se faire des amis',3,3,3,1,1),
+('Tintin au pays des Soviets',4,4,4,1,1)
 ;
+
+INSERT INTO collection(nom,infos,idediteur) VALUES
+
+('inconnu','inconnu',1),
+('ONE PIECE','L histoire suit principalement l équipage de Chapeau de paille, mené par son capitaine Monkey D. Luffy, 
+un jeune homme ayant mangé le fruit du Gum Gum et dont le rêve est de devenir le Roi des pirates.',2),
+('TWO PIECE','L histoire suit principalement l équipage de Chapeau de paille, mené par son capitaine Monkey D. Loufi, 
+un jeune homme ayant mangé le fruit du Gum Gum et dont le rêve est de devenir le Roi des pirates.',2),
+('Tintin','Le T',4)
+;
+
+
 
 
 #INSERTION AUTEUR
 #Insertion par défaut
-INSERT INTO auteur(id,nom) VALUES (0,'NO_NAME');
+INSERT INTO auteur(id,nom) VALUES ('NULL','inconnu');
 #Insertion des auteurs
 INSERT INTO auteur(nom) VALUES 
 ('Eichiro Oda'),
@@ -141,9 +185,19 @@ INSERT INTO auteur(nom) VALUES
 ;
 
 
+#INSERTION PERSONNAGES
+#Insertion des auteurs
+INSERT INTO personnage(nom,idauteur,idcollection) VALUES 
+('Luffy', 2,2),
+('Loufi', 2,3),
+('Tintin', 4, 4),
+('Milou', 4, 4)
+;
+
+
 #INSERTION EDITEUR
 #Insertion par défaut
-INSERT INTO editeur(nom) VALUES ('NO_NAME');
+INSERT INTO editeur(id,nom) VALUES ('NULL','inconnu');
 #Insertion des editeurs
 INSERT INTO editeur(nom) VALUES
 ('Glénat'),
@@ -154,7 +208,7 @@ INSERT INTO editeur(nom) VALUES
 
 #INSERTION GENRE
 #Insertion par défaut
-INSERT INTO genre(nom) VALUES ('NO_NAME');
+INSERT INTO genre(id,nom) VALUES ('NULL','inconnu');
 #Insertion des genres
 INSERT INTO genre(nom) VALUES
 ('Shonen'),
